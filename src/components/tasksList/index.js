95de72bf93a2d5149d9c36ed3c926786a9165176
popensislam <s.islam.like@mail.ru>
2022-09-25 02:39:48 +0500

    adaptive

--- src/components/tasksList/index.js
+++ src/components/tasksList/index.js
@@ -83,7 +83,7 @@ const TasksList = ({ isResult, makeBold, importPage, checkTask, tasks, deletedTa
                         onDrop={e => dropHandler(e, task)}
                         draggable={true}
                     >
-                        <div className='item-flex'>
+                        <div className='item-flex title'>
                             <Checkbox id={task.id} checked={task.isCompleted} onChangeCheck={() => onChangeCompleted(task)} />
                             {
                                 isResult
@@ -106,26 +106,28 @@ const TasksList = ({ isResult, makeBold, importPage, checkTask, tasks, deletedTa
                                     </span>
                             }
                         </div>
-                        <div className='item-flex'>
-                            {task.productiveTag && (
-                                <span className='tag-item productive'>Продуктивность</span>
-                            )}
-                            {task.educationTag && (
-                                <span className='tag-item education'>Образование</span>
-                            )}
-                            {task.healthTag && (
-                                <span className='tag-item health'>Здоровье</span>
-                            )}
-                            {task.importantTag && (
-                                <span className='tag-item important'>Срочно</span>
-                            )}
-                            <span style={{ color: '#676D7D' }}>{task?.date}</span>
+                        <div className='item-flex atributes'>
+                            <div className='item-flex-tags'>
+                                {task.productiveTag && (
+                                    <span className='tag-item productive'>Продуктивность</span>
+                                )}
+                                {task.educationTag && (
+                                    <span className='tag-item education'>Образование</span>
+                                )}
+                                {task.healthTag && (
+                                    <span className='tag-item health'>Здоровье</span>
+                                )}
+                                {task.importantTag && (
+                                    <span className='tag-item important'>Срочно</span>
+                                )}
+                            </div>
+                            <span className='item-flex-date' style={{ color: '#676D7D' }}>{task?.date}</span>
                             {
                                 deletedPage
                                     ?
                                     <img className='reset' onClick={() => deletedTask(task)} src={resetIcon} alt='resets icon' />
                                     :
-                                    <>
+                                    <div className='item-flex-icons'>
                                         <svg onClick={() => deletedTask(task)} style={{ cursor: 'pointer' }} width="20" height="20" viewBox="0 0 20 20" fill="#BDBDBD" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M17.5 4.16671H14.1666V3.33337C14.1666 1.91671 13.0833 0.833374 11.6666 0.833374H8.33329C6.91663 0.833374 5.83329 1.91671 5.83329 3.33337V4.16671H2.49996C1.99996 4.16671 1.66663 4.50004 1.66663 5.00004C1.66663 5.50004 1.99996 5.83337 2.49996 5.83337H3.33329V16.6667C3.33329 18.0834 4.41663 19.1667 5.83329 19.1667H14.1666C15.5833 19.1667 16.6666 18.0834 16.6666 16.6667V5.83337H17.5C18 5.83337 18.3333 5.50004 18.3333 5.00004C18.3333 4.50004 18 4.16671 17.5 4.16671ZM7.49996 3.33337C7.49996 2.83337 7.83329 2.50004 8.33329 2.50004H11.6666C12.1666 2.50004 12.5 2.83337 12.5 3.33337V4.16671H7.49996V3.33337ZM14.1666 17.5C14.6666 17.5 15 17.1667 15 16.6667V5.83337H4.99996V16.6667C4.99996 17.1667 5.33329 17.5 5.83329 17.5H14.1666Z" fill="black" />
                                             <mask id="mask0_18_950" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="1" y="0" width="18" height="20">
@@ -136,7 +138,7 @@ const TasksList = ({ isResult, makeBold, importPage, checkTask, tasks, deletedTa
                                             </g>
                                         </svg>
                                         <img src={dropDownIcon} alt='drop downs icon' />
-                                    </>
+                                    </div>
                             }
                         </div>
                     </div>
@@ -146,7 +148,7 @@ const TasksList = ({ isResult, makeBold, importPage, checkTask, tasks, deletedTa
                 tasks.length > taskLength && (
                     <div className='more' onClick={() => setTaskLength(prev => prev + 5)}>Показать еще 5</div>
 
-            )}
+                )}
         </div>
     );
 }
